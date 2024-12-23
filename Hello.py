3c2c15aa5774e2230c14465a0e6966d4d87ac3ce
Dinithi Kavindya <dinithikavi2002@gmail.com>
2024-12-23 17:45:47 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,24 +1,22 @@
-import time
+n = 10
+# Open a file in write mode
+with open("christmas_tree.txt", "w") as file:
+    for i in range(0, n):
+        # Create a line for the current row
+        line = ""
+        for j in range(1, n - i):
+            line += " "  # Add spaces
+        line += "*"  # Add the first star
+        for k in range(0, i):
+            line += "!"  # Add exclamation marks on the left
+        for j in range(1, i):
+            line += "!"  # Add exclamation marks on the right
+        if i > 0:
+            line += "*"  # Add the last star
+        line += "\n"  # Move to the next line
+        file.write(line)  # Write the line to the file
 
-for i in range(10,0,-1):
-    print(i)
-    time.sleep(1)
-print("MERRY CHRISTMAS")
-
-      
-    
-n=10
-for i in range(0, n):
-    for j in range(1,n-i):
-        print(" ",end="")
-    print("*",end="")
-    for k in range(0,i):
-        print("!",end="")
-    for j in range(1,i):
-        print("!",end="")
-    if i>0:
-        print("*", end="")
-    print()    
+print("Pattern has been written to 'christmas_tree.txt'.")
 
 
 
@@ -26,25 +24,3 @@ for i in range(0, n):
 
 
 
-
-
-
-
-
-
-import turtle
-import colorsys
-t=turtle.Turtle()
-s=turtle.Screen().bgcolor("black")
-t.speed(-5)
-n=70
-h=0
-for i in range(360):
-    c=colorsys.hsv_to_rgb(h,1,0.8)
-    h+=1/n
-    t.color(c)
-    t.left(1)
-    t.fd(1)
-    for j in range(2):
-        t.left(2)
-        t.circle(100)
